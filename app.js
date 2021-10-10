0a9adf9e430b99b428125ea2e3088517e499e8d6
Victor Hugo Pongolino <victor.pongolino@hotmail.com>
2021-10-10 20:07:28 -0300

    adicionando rotas

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-const app = require("./app/config/config");
+const app = require("./config/config");
 
 const porta = 3000;
 app.listen(porta, () => {
     console.log(`Servidor escutando na porta ${porta}`)
 });
+
+const homeIndexEJS = require("./app/routes/index");
+homeIndexEJS(app);
--- config/config.js
+++ config/config.js
@@ -1,8 +1,9 @@
 var express = require('express'); // Retorna uma função.
 
-
 var app = express();
 app.set("view engine", "ejs"); // Engine que irá processar os htmls
-app.set("views", "../views"); // Local aonde as views estão.
+app.set("views", "./app/views/"); // Local aonde as views estão.
+
+console.log("Configurações carregadas!");
 
 module.exports = app;
